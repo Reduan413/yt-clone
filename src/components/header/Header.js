7c7c5f729ae6fa0c64a2b17184315b73341489f9
reduan <reduan15-11413@diu.edu.bd>
2021-03-03 01:33:54 +0600

    video-page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -25,7 +25,7 @@ const Header = ({ handleToggleSidebar }) => {
                 <MdNotifications size={28} />
                 <MdApps size={28} />
                 <img
-                    src="https://cdn.icon-icons.com/icons2/1879/PNG/512/iconfinder-8-avatar-2754583_120515.png"
+                    src="https://cdn.iconscout.com/icon/free/png-512/avatar-370-456322.png"
                     alt="avater"
                 />
             </div>
--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,9 +1,27 @@
 import React from 'react'
-import './_video.sass'
+import './_video.scss'
+import { AiFillEye } from 'react-icons/ai'
+
 const video = () => {
     return (
-        <div>
-            <h1>video</h1>
+        <div className="video">
+            <div className="video__top">
+                <img src="https://i.ytimg.com/vi/AwRb8LX-szg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBmgMg9_elE9IV7_ZGK19klZaGMQQ" alt="" />
+                <span>38:08</span>
+            </div>
+            <div className="video__title">
+                Responsive Animated Product Card Using HTML & CSS & JavaScript
+            </div>
+            <div className="video__details">
+                <span>
+                    <AiFillEye/> 5m Views •
+                </span>
+                <span> 6 days ago</span>
+            </div>
+            <div className="video__channel">
+                <img src="https://yt3.ggpht.com/ytc/AAUvwnjjl7sUdeyZ4pGOobMQnFM-U91gh1rZ40hvg1NONQ=s48-c-k-c0x00ffffff-no-rj" alt="" />
+                <p>True Coder</p>
+            </div>
         </div>
     )
 }
